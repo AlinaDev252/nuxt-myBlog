602bfe67fe059846dc232760e366e1c1e6d9e7fc
AlinaDev252 <costachealina25@gmail.com>
2022-03-22 12:44:53 +0100

    Split the page into Components

--- pages/index.vue
+++ pages/index.vue
@@ -4,29 +4,39 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <NuxtLink :to="'/posts/' + 1 " class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://cdn.pixabay.com/photo/2020/08/09/14/25/business-5475661_1280.jpg')">
-            <div class="post-content">
-              <h1>Post Title</h1>
-              <p>Preview Text</p>
-            </div>
-          </div>
-        </article>
-      </NuxtLink>
-       <NuxtLink :to="'/posts/' + 2 " class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://cdn.pixabay.com/photo/2020/08/09/14/25/business-5475661_1280.jpg')">
-            <div class="post-content">
-              <h1>Post Title 2</h1>
-              <p>Preview Text 2</p>
-            </div>
-          </div>
-        </article>
-      </NuxtLink>
+      <PostPreview
+        id="1"
+        thumbnail="https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
+        title="Hello there!"
+        previewText="This is my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
+        title="Hello there - the second time!"
+        previewText="This is my second post"
+       />
+      <PostPreview
+        id="3"
+        thumbnail="https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
+        title="Hi!"
+        previewText="This is my third post"
+
+       />
     </section>
   </div>
 </template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+  components: {
+    PostPreview,
+  }
+}
+</script>
+
 
 <style scoped>
 .intro {
@@ -68,39 +78,4 @@
   justify-content: center;
 }
 
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
-}
 </style>
