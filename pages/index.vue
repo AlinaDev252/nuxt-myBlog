f1507f21073b9720b7199081b74c95226a66d51b
AlinaDev252 <costachealina25@gmail.com>
2022-03-24 12:15:49 +0100

    Add nuxtServerInit to vuex actions

--- pages/index.vue
+++ pages/index.vue
@@ -14,36 +14,11 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-           {
-            id: "1",
-            title: "First Post",
-            previewText: "This is our first post!",
-            thumbnail:
-            "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
-           },
-           {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post!",
-            thumbnail:
-            "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
-           }
-          ]
-       });
-    }, 1000);
-  })
-  .then(data => {
-    return data
-  })
-  .catch(e => {
-      context.error(new Error())
-    })
-  },
+  computed: {
+    loadedPosts(){
+      return this.$store.getters.loadedPosts
+    }
+  }
 }
   // data() {
   //   return {
--- store/index.js
+++ store/index.js
@@ -11,16 +11,39 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('setPosts', [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is our first post!",
+                thumbnail:
+                  "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is our second post!",
+                thumbnail:
+                  "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
+              }
+            ]);
+            resolve();
+          }, 1000)
+        });
+      },
       setPosts(vuexContext, posts) {
-        vuexContext.commit('setPosts', posts)
+        vuexContext.commit('setPosts', posts);
       }
     },
     getters: {
       loadedPosts(state) {
-        return state.loadedPosts
+        return state.loadedPosts;
       }
     }
-  })
-}
+  }
+)}
 
 export default createStore
