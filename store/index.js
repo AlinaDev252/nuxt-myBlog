25416721b8deadfba3dfb6778262cf68616648ed
AlinaDev252 <costachealina25@gmail.com>
2022-03-24 23:37:34 +0100

    Fetch data from Firestore

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,27 +13,15 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setPosts', [
-              {
-                id: "1",
-                title: "First Post",
-                previewText: "This is our first post!",
-                thumbnail:
-                  "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
-              },
-              {
-                id: "2",
-                title: "Second Post",
-                previewText: "This is our second post!",
-                thumbnail:
-                  "https://media.istockphoto.com/photos/abstract-blue-digital-background-picture-id1146532466?k=20&m=1146532466&s=612x612&w=0&h=NjZrRzJH4nvxVmTGTvMrMrPGQ03fDNYTmRNoEiNSeCQ="
-              }
-            ]);
-            resolve();
-          }, 1000)
-        });
+        return axios.get("https://nuxt-blog-63bb3-default-rtdb.firebaseio.com/posts.json")
+          .then(res => {
+            const postsArray = []
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
+            vuexContext.commit('setPosts', postsArray)
+          })
+          .catch(e => context.error(e));
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts);
