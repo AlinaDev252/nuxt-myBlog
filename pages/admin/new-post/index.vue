9fdcb5b102151e6acd20fcd82dca994f4d41634a
AlinaDev252 <costachealina25@gmail.com>
2022-03-24 22:59:51 +0100

    Use Firebase to store the data

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
@@ -14,7 +15,14 @@ export default {
   components: {
     AdminPostForm
   },
-}
+  methods: {
+    onSubmitted(postData) {
+      axios.post('https://nuxt-blog-63bb3-default-rtdb.firebaseio.com/posts.json', postData)
+        .then(result => console.log(result))
+        .catch(e => console.log(e))
+    }
+  }
+};
 </script>
 
 <style scoped>
